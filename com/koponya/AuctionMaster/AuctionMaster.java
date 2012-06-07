00da1c7b184eadea849264613f0e875ac28834f9
Koponya <gaga1004@gmail.com>
2012-06-07 05:49:09 +0200

    add data file and create save and load config method

--- com/koponya/AuctionMaster/AuctionMaster.java
+++ com/koponya/AuctionMaster/AuctionMaster.java
@@ -1,33 +1,64 @@
 package com.koponya.AuctionMaster;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.Configuration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuctionMaster extends JavaPlugin {
 
-	public Logger log;
-	public Configuration conf;
-	public final String pluginName = this.getDescription().getName();
+	private Logger log;
+	public FileConfiguration conf;
+	public FileConfiguration data;
+	public String pluginName;
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		return AuctionCommands.onCommand(sender, cmd, label, args);
 	}
 	
-	public void onEnabled() {
-		this.log = this.getLogger();
-		this.conf = this.getConfig();
-		this.conf.options().copyDefaults(true);
-		Lang.init("hu");
-		
-		log.info("["+pluginName+"] Plugin enabled!");
+	public void infoLog(String msg) {
+		log.info(msg);
+	}
+
+	public void warningLog(String msg) {
+		log.warning(msg);
 	}
 	
-	public void onDisabled() {
-		log.info("["+pluginName+"] Plugin disabled!");
+	public void loadConfig() {
+		this.conf = this.getConfig();
+		this.data = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), "data.yml"));
+	}
+	
+	@Override
+	public void saveConfig() {
+		super.saveConfig();
+		try {
+			this.data.save(new File(this.getDataFolder(), "data.yml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void onLoad() {
+		this.log = this.getLogger();
+		this.pluginName = this.getDescription().getName();
+		this.loadConfig();
+		this.saveConfig();
+	}
+	
+	public void onEnable() {
+		Lang.init(this.conf.getString("language"));
+		infoLog("Plugin enabled!");
+	}
+	
+	public void onDisable() {
+		this.saveConfig();
+		infoLog("Plugin disabled!");
 	}
 }
