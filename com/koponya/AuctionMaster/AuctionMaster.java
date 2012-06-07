c97aa4588b0fd40f5d36b11d96e92547b31d0319
Koponya <gaga1004@gmail.com>
2012-06-07 07:14:10 +0200

    create copydefault for confit and load the language file

--- com/koponya/AuctionMaster/AuctionMaster.java
+++ com/koponya/AuctionMaster/AuctionMaster.java
@@ -1,7 +1,10 @@
 package com.koponya.AuctionMaster;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.logging.Logger;
 
 import org.bukkit.command.Command;
@@ -37,24 +40,43 @@ public class AuctionMaster extends JavaPlugin {
 	
 	@Override
 	public void saveConfig() {
-		super.saveConfig();
+		//super.saveConfig();
 		try {
 			this.data.save(new File(this.getDataFolder(), "data.yml"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+	private void copyDefault(String name) {
+        try {
+        	File file = new File(this.getDataFolder(),name);
+        	if(file.exists()) return;
+        	InputStream in = this.getResource(name);
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 	
 	public void onLoad() {
 		this.log = this.getLogger();
 		this.pluginName = this.getDescription().getName();
 		this.loadConfig();
 		this.saveConfig();
+		copyDefault("hu.lang");
 	}
 	
 	public void onEnable() {
 		Lang.init(this.conf.getString("language"));
-		infoLog("Plugin enabled!");
+		infoLog(Lang.get("console.welcome"));
 	}
 	
 	public void onDisable() {
--- com/koponya/AuctionMaster/Lang.java
+++ com/koponya/AuctionMaster/Lang.java
@@ -1,5 +1,9 @@
 package com.koponya.AuctionMaster;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,12 +11,38 @@ public class Lang {
 
 	private Lang() {}
 	private static final Map<String,String> language = new HashMap<String, String>(); 
-	
+
 	public static boolean init(String lang) {
 		if(!Lang.language.isEmpty())
 			return false;
-		
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File("plugins/AuctionMaster/hu.lang")), "UTF8"));
+			String line;
+			while((line=in.readLine())!=null) {
+				try {
+					String[] word = line.split("=", 2);
+					Lang.language.put(word[0], word[1]);
+				} catch (Exception ex) { }
+			}
+			in.close();
+			if(!lang.equalsIgnoreCase("hu")) {
+				in = new BufferedReader(new InputStreamReader(new FileInputStream(new File("plugins/AuctionMaster/"+lang+".lang")), "UTF8"));
+				while((line=in.readLine())!=null) {
+					try {
+						String[] word = line.split("=", 2);
+						Lang.language.put(word[0], word[1]);
+					} catch (Exception ex) { }
+				}
+				in.close();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		return false;
+	}
+	
+	public static String get(String key) {
+		return language.get(key);
 	}
 	
 }
